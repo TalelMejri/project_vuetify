e3a602974d67df48d027c48ba35f3eb214742474
talel <talelmejri8@gmail.com>
2022-09-02 22:54:31 +0100

    init project

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,7 @@
 const { defineConfig } = require('@vue/cli-service')
+
 module.exports = defineConfig({
-  transpileDependencies: [
-    'vuetify'
-  ]
+  lintOnSave:false,
+  transpileDependencies: true
 })
+
