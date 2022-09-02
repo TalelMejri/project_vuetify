d8c64546e84be6227951d3907923ed9c2505097b
talel <talelmejri8@gmail.com>
2022-09-02 22:36:52 +0100

    add vuetify

---
+++ src/plugins/vuetify.js
@@ -0,0 +1,28 @@
+import Vue from 'vue';
+import Vuetify from 'vuetify/lib/framework';
+import ar from 'vuetify/lib/locale/ar';
+
+Vue.use(Vuetify);
+
+export default new Vuetify({
+  theme: {
+      options: {
+        customProperties: true,
+      },
+    themes: {
+      light: {
+        primary: '#007BFF',
+        secondary: '#424242',
+        accent: '#82B1FF',
+        error: '#FF5252',
+        info: '#2196F3',
+        success: '#4CAF50',
+        warning: '#FFC107'
+      },
+    },
+  },
+    lang: {
+      locales: { ar },
+      current: 'ar',
+    },
+});
