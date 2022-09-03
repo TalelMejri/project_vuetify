e37139016f62bf4f134fc428c30a705658935c21
talel <talelmejri8@gmail.com>
2022-09-03 10:39:07 +0100

    test confetti

--- src/App.vue
+++ src/App.vue
@@ -6,6 +6,9 @@
     <span  class="d-flex justify-center font-weight-bold display-4 ">hello</span>
     
     <div>
+      <v-btn @click="start">Start</v-btn>
+      <v-btn  @click="stop">Stop</v-btn >
+      <v-btn  @click="love">Show some love</v-btn >
       
       <v-btn  fab class="hidden-md-only">hello</v-btn>
     </div>  
@@ -15,6 +18,10 @@
 
 <script>
 import navbar from "@/components/Navbar.vue";
+import Vue from 'vue'
+import VueConfetti from 'vue-confetti'
+
+Vue.use(VueConfetti)
 export default {
   name: 'App',
   components:{
@@ -24,5 +31,32 @@ export default {
   data: () => ({
     //
   }),
+  methods: {
+      start() {
+        this.$confetti.start();
+      },
+
+      stop() {
+        this.$confetti.stop();
+      },
+
+      love() {
+        this.$confetti.update({
+          particles: [
+            {
+              type: 'heart',
+            },
+            {
+              type: 'circle',
+            },
+          ],
+          defaultColors: [
+            'red',
+            'pink',
+            '#ba0000'
+          ],
+        });
+      }
+    }
 };
 </script>
