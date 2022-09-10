9e61f69b552f79b36bfa91f6ac7364d4e12ade5e
talel <talelmejri8@gmail.com>
2022-09-10 14:59:06 +0100

    grid system

--- src/views/accueil.vue
+++ src/views/accueil.vue
@@ -1,5 +1,28 @@
 <template>
     <div class="home">
-        Home
+        <v-container class="py-5">
+            <v-layout row wrap justify-space-between>
+                <v-flex xs12 md12>
+                    <v-btn outline block class="primary">
+                        <v-icon>mdi-facebook</v-icon>
+                    </v-btn>
+                </v-flex>
+                <v-flex xs6 md6>
+                    <v-btn outline block class="primary">
+                        <v-icon>mdi-facebook</v-icon>
+                    </v-btn>
+                </v-flex>
+                <v-flex xs4 md2>
+                    <v-btn outline block class="primary">
+                        <v-icon>mdi-twitter</v-icon>
+                    </v-btn>
+                </v-flex>
+                <v-flex xs4 md2>
+                    <v-btn outline block class="danger">
+                        <v-icon>mdi-youtube</v-icon>
+                    </v-btn>
+                </v-flex>
+            </v-layout>
+        </v-container>
     </div>
 </template>
